b92b13a96d615eb45104f523ad363670c76e2229
Stefan Rusu <saltwaterc@gmail.com>
2012-02-06 15:18:31 +0200

    Kernel purge script for Ubuntu on EC2.

---
+++ suites/aws-tools/ubuntu-ec2-kernel-purge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# purge the linux-image-virtual obsolete packages
+version=$(ls /boot/ | grep vmlinuz | grep generic | cut -d"-" -f2 | sort -u)
+declare -a installed=($(ls /boot/ | grep vmlinuz | grep generic | cut -d"-" -f3 | sort -u))
+count=${#installed[@]}
+
+for ((i = 0; i < count - 1; i++))
+do
+	apt-get -y autoremove --purge linux-image-$version-${installed[$i]}-virtual
+done
+
+# purge the linux-image-ec2 packages that are not in use
+version=$(ls /boot/ | grep vmlinuz | grep ec2 | cut -d"-" -f2 | sort -u)
+declare -a installed=($(ls /boot/ | grep vmlinuz | grep ec2 | cut -d"-" -f3 | sort -u))
+count=${#installed[@]}
+
+for ((i = 0; i < count; i++))
+do
+	if [ "$version-${installed[$i]}-ec2" != "$(uname -r)" ]
+	then
+		apt-get -y autoremove --purge linux-image-$version-${installed[$i]}-ec2
+	fi
+done
